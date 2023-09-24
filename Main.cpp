79a021503def844ec26d4d74fb738a22fc6ed26e
JoseEduardoMartinezUlloa <jose.martinez5525@alumnos.udg.mx>
2023-09-24 17:51:23 -0600

    MenuCambiado

--- Main.cpp
+++ Main.cpp
@@ -10,49 +10,35 @@ int main() {
 
     int opcion;
     do {
-        std::cout << "\nMenu:\n";
+        std::cout << "\nMenu - Sistema Control Escolar\n";
         std::cout << "1. Agregar Alumno\n";
-        std::cout << "2. Eliminar Alumno\n";
-        std::cout << "3. Imprimir Alumnos\n";
-        std::cout << "4. Ordenar Alumnos\n";
-        std::cout << "5. Salir\n";
+        std::cout << "2. Imprimir Alumnos\n";
+        std::cout << "3. Transformar la lista original (intercalar)\n";
+        std::cout << "4. Salir\n";
         std::cout << "Ingrese la opción: ";
         std::cin >> opcion;
 
         switch (opcion) {
             case 1: 
-                
                 std::cin>>alumno;
                 listaAlumnos.insertar(alumno);
                 break;
-            case 2: 
-                std::cout << "Dame el nombre del alumno: ";
-                std::cin >> nom;
-                AlumEliminar.setNombre(nom);     // Establece el nombre proporcionado
-                
-                pos = listaAlumnos.Busqueda(AlumEliminar);
-                if (pos != nullptr) {
-                    listaAlumnos.eliminar(pos);
-                } else {
-                    std::cout << "Alumno no encontrado.\n";
-                }
-                break;
-            case 3:
+            case 2:
                 std::cout << "Lista de Alumnos:\n";
                 listaAlumnos.imprimir();
                 break;
-            case 4:
+            case 3:
                 listaAlumnos.Ordenamiento();
                 std::cout << "Alumnos ordenados por género.\n";
                 break;
-            case 5:
+            case 4:
                 std::cout << "Saliendo del programa.\n";
                 break;
             default:
                 std::cout << "Opción inválida. Intente de nuevo.\n";
                 break;
         }
-    } while (opcion != 5);
+    } while (opcion != 4);
 
     return 0;
 }
